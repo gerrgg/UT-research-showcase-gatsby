6a6539585957d70ee08f5d889f73753cdcf4b249
Greg Bastianelli <greg@gerrg.com>
2020-11-11 11:49:03 -0500

    format date and add school to index

--- src/pages/index.js
+++ src/pages/index.js
@@ -2,6 +2,7 @@ import React, { useState } from "react"
 import "../sass/style.scss"
 import { Link, graphql } from "gatsby"
 import Layout from "../components/layout"
+import moment from "moment"
 
 export default function Home({ data }) {
   const [filter, setFilter] = useState("")
@@ -36,17 +37,29 @@ const Filter = ({ filter, setFilter }) => (
 
 const Title = () => <h1 className="title">UT Research Articles</h1>
 const PostCount = ({ count }) => <h4>{count} Posts</h4>
+const PostDate = ({ date }) => (
+  <span> — {moment(date).format("MMM Do YYYY")}</span>
+)
+
+const PostCategory = ({ category }) => {
+  const categories = category.split("|")
+  return <p className="category">{categories[0]}</p>
+}
 
 const Post = ({ post }) => {
-  console.log(post)
+  console.log(post.frontmatter)
   return (
     <div className="blog">
       <Link to={post.fields.slug}>
         <h3>
-          {post.frontmatter.title} <span>— {post.frontmatter.date}</span>
+          {post.frontmatter.title} <PostDate date={post.frontmatter.date} />
         </h3>
         <p>{post.excerpt}</p>
-        <p className="timeToRead">{post.timeToRead} minute read</p>
+
+        <div className="postmeta">
+          <p className="timeToRead">{post.timeToRead} minute read</p>
+          <PostCategory category={post.frontmatter.category} />
+        </div>
       </Link>
     </div>
   )
@@ -62,6 +75,7 @@ export const query = graphql`
           frontmatter {
             title
             date
+            category
           }
           fields {
             slug
